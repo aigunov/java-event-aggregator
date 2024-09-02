610e5fe7f422b910e4ff35b7ba3aabb80c4dc6ad
MrWhiteShapi <aigunovshapi@yandex.ru>
2024-09-02 03:32:38 +0300

    logic refactoring

--- stats/stats-server/src/main/java/ru/practicum/statsserver/Mapper.java
+++ stats/stats-server/src/main/java/ru/practicum/statsserver/Mapper.java
@@ -2,7 +2,6 @@ package ru.practicum.statsserver;
 
 import lombok.experimental.UtilityClass;
 import ru.practicum.statsdto.dto.StatDto;
-import ru.practicum.statsdto.dto.ViewStats;
 
 @UtilityClass
 public class Mapper {
@@ -32,14 +31,4 @@ public class Mapper {
                 .build();
     }
 
-    /**
-     * Object --> ViewStats
-     */
-    ViewStats toViewStats(Object[] object) {
-        return ViewStats.builder()
-                .app((String) object[0])
-                .uri((String) object[1])
-                .hit((Long) object[2])
-                .build();
-    }
 }
--- stats/stats-server/src/main/java/ru/practicum/statsserver/ServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/statsserver/ServiceImpl.java
@@ -7,7 +7,10 @@ import ru.practicum.statsdto.dto.StatDto;
 import ru.practicum.statsdto.dto.ViewStats;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -25,10 +28,28 @@ public final class ServiceImpl implements StatService {
     @Override
     public List<ViewStats> getViewStats(final LocalDateTime start, final LocalDateTime end,
                                         final List<String> uris, final Boolean unique) {
-        List<ViewStats> viewStats = unique ?
-                repo.getViewStatisticsUnique(start, end, uris).stream().map(Mapper::toViewStats).toList()
-                : repo.getViewStatistics(start, end, uris).stream().map(Mapper::toViewStats).toList();
-        log.info("viewStats {}", viewStats);
-        return viewStats;
+        List<ViewStats> viewStatsList = new ArrayList<>();
+        if (uris == null) {
+            if (unique) {
+                viewStatsList = repo.getAllViewStats(start, end);
+            } else {
+                viewStatsList = repo.getAllViewStatsUnique(start, end);
+            }
+        } else {
+            if (unique) {
+                for (String s : uris) {
+                    ViewStats viewStats = repo.getViewStatsUnique(start, end, s);
+                    viewStatsList.add(Objects.requireNonNullElseGet(viewStats, () -> new ViewStats("ewm-main-service", s, 0L)));
+                }
+            } else {
+                for (String s : uris) {
+                    ViewStats viewStats = repo.getViewStats(start, end, s);
+                    viewStatsList.add(Objects.requireNonNullElseGet(viewStats, () -> new ViewStats("ewm-main-service", s, 0L)));
+                }
+            }
+        }
+        viewStatsList.sort(Comparator.comparing(ViewStats::getHits).reversed());
+        log.info("GET /stats -> returning from db {}", viewStatsList);
+        return viewStatsList;
     }
 }
