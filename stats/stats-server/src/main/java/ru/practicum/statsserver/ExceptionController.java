4ac8f50aace023d04657d8cf1a9fab1cb673fc18
MrWhiteShapi <aigunovshapi@yandex.ru>
2024-09-13 17:52:06 +0300

    Все тесты stats-service проходит

---
+++ stats/stats-server/src/main/java/ru/practicum/statsserver/ExceptionController.java
@@ -0,0 +1,20 @@
+package ru.practicum.statsserver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice("ru.practicum")
+@Slf4j
+public class ExceptionController {
+
+    @ExceptionHandler(NoValidParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String,String> handleNOValidParameter(RuntimeException e) {
+        return Map.of("Ошибка: ", e.getMessage());
+    }
+}
